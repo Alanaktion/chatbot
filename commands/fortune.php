27e8e24bc8e3f7c7e37c3551a86d03c89314c82d
Alan Hardman <me@imalan.tk>
2013-07-03 16:47:27 -0600

    Adding #fortune command.

---
+++ commands/fortune.php
@@ -0,0 +1,11 @@
+<?php
+$commands['fortune'] = function(&$conn, $event, $params) {
+	if (!empty($params[0])) {
+		$url = "raw&q=" . $params[0];
+	} else {
+		$url = "raw";
+	}
+	$response = curl_get_contents("http://alanaktion.net/fun/fortune.php?" . $url);
+	$conn->message($event['from'], $response, $event['type']);
+}
+?>
