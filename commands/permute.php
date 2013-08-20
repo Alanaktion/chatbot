a2adb73861ce43d421e273198d9f84d863cec108
Alan Hardman <me@imalan.tk>
2013-08-20 17:05:26 -0600

    Adding a lot more big named numbers to permutation info.

--- commands/permute.php
+++ commands/permute.php
@@ -36,18 +36,74 @@ $commands['permute'] = function(&$conn, $pl, $params) {
 		$param_str = implode(" ", $params);
 		if (strlen($param_str) > 6) {
 			$n = factorial(strlen($param_str));
-			if($n>1000000000000000)
-				$str_count = round(($n/1000000000000000),1).' quadrillion';
-			elseif($n>1000000000000)
-				$str_count = round(($n/1000000000000),1).' trillion';
-			elseif($n>1000000000)
-				$str_count = round(($n/1000000000),1).' billion';
+			// Start with a Googol and work down
+			if($n>pow(10,100))
+				$str_count = round(($n/pow(10,100)),1).' googol';
+			// I'll add more later
+			elseif($n>pow(1000,32))
+				$str_count = round(($n/pow(1000,32)),1).' untrigintillion';
+			elseif($n>pow(1000,31))
+				$str_count = round(($n/pow(1000,31)),1).' trigintillion';
+			elseif($n>pow(1000,30))
+				$str_count = round(($n/pow(1000,30)),1).' novemvigintillion';
+			elseif($n>pow(1000,29))
+				$str_count = round(($n/pow(1000,29)),1).' octovigintillion';
+			elseif($n>pow(1000,28))
+				$str_count = round(($n/pow(1000,28)),1).' septenvigintillion';
+			elseif($n>pow(1000,27))
+				$str_count = round(($n/pow(1000,27)),1).' sexvigintillion';
+			elseif($n>pow(1000,26))
+				$str_count = round(($n/pow(1000,26)),1).' quinvigintillion';
+			elseif($n>pow(1000,25))
+				$str_count = round(($n/pow(1000,25)),1).' quattuorvigintillion';
+			elseif($n>pow(1000,24))
+				$str_count = round(($n/pow(1000,24)),1).' trevigintillion';
+			elseif($n>pow(1000,23))
+				$str_count = round(($n/pow(1000,23)),1).' duovigintillion';
+			elseif($n>pow(1000,22))
+				$str_count = round(($n/pow(1000,22)),1).' unvigintillion';
+			elseif($n>pow(1000,21))
+				$str_count = round(($n/pow(1000,21)),1).' vigintillion';
+			elseif($n>pow(1000,20))
+				$str_count = round(($n/pow(1000,20)),1).' novemdecillion';
+			elseif($n>pow(1000,19))
+				$str_count = round(($n/pow(1000,19)),1).' octodecillion';
+			elseif($n>pow(1000,18))
+				$str_count = round(($n/pow(1000,18)),1).' septendecillion';
+			elseif($n>pow(1000,17))
+				$str_count = round(($n/pow(1000,17)),1).' sexdecillion';
+			elseif($n>pow(1000,16))
+				$str_count = round(($n/pow(1000,16)),1).' quindecillion';
+			elseif($n>pow(1000,15))
+				$str_count = round(($n/pow(1000,15)),1).' quattuordecillion';
+			elseif($n>pow(1000,14))
+				$str_count = round(($n/pow(1000,14)),1).' tredecillion';
+			elseif($n>pow(1000,13))
+				$str_count = round(($n/pow(1000,13)),1).' duodecillion';
+			elseif($n>pow(1000,12))
+				$str_count = round(($n/pow(1000,12)),1).' undecillion';
+			elseif($n>pow(1000,11))
+				$str_count = round(($n/pow(1000,11)),1).' decillion';
+			elseif($n>pow(1000,10))
+				$str_count = round(($n/pow(1000,10)),1).' nonillion';
+			elseif($n>pow(1000,9))
+				$str_count = round(($n/pow(1000,9)),1).' octillion';
+			elseif($n>pow(1000,8))
+				$str_count = round(($n/pow(1000,8)),1).' septillion';
+			elseif($n>pow(1000,7))
+				$str_count = round(($n/pow(1000,7)),1).' sextillion';
+			elseif($n>pow(1000,6))
+				$str_count = round(($n/pow(1000,6)),1).' quintillion';
+			elseif($n>pow(1000,5))
+				$str_count = round(($n/pow(1000,5)),1).' quadrillion';
+			elseif($n>pow(1000,4))
+				$str_count = round(($n/pow(1000,4)),1).' trillion';
+			elseif($n>pow(1000,3))
+				$str_count = round(($n/pow(1000,3)),1).' billion';
 			elseif($n>1000000)
 				$str_count = round(($n/1000000),1).' million';
 			elseif($n>1000)
 				$str_count = round(($n/1000),1).' thousand';
-			else
-				$str_count = number_format($n);
 
 			$conn->message($pl['from'], "That word has " . $str_count . " permutations. That's a lot.", $pl['type']);
 		} else {
