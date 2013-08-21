e28b1d3d71aa76586e08ef9c5771baef09ccafe4
Alan Hardman <me@imalan.tk>
2013-08-21 16:26:52 -0600

    Adding additional font support to #shout.

--- commands/shout.php
+++ commands/shout.php
@@ -2,11 +2,23 @@
 $commands['shout'] = function(&$conn, $pl, $params) {
 	global $mash_key;
 	if (!empty($params[0])) {
-		$param_str = implode(" ",$params);
-		$response = curl_get_contents("https://artii.herokuapp.com/make?text=" . urlencode($param_str));
-		$conn->message($pl['from'], "\n" . $response, $pl['type']);
+		if($params[0] == "--fonts") {
+			$response = curl_get_contents("https://artii.herokuapp.com/fonts_list");
+			$response = implode(", ", explode("\n", $response));
+			$conn->message($pl['from'], "\n" . $response, $pl['type']);
+		} elseif(substr($params[0], 0, 5) == "font=") {
+			$font = substr($params[0], 5);
+			unset($params[0]);
+			$param_str = implode(" ",$params);
+			$response = curl_get_contents("https://artii.herokuapp.com/make?text=" . urlencode($param_str) . "&font=" . urlencode($font));
+			$conn->message($pl['from'], "\n" . $response, $pl['type']);
+		} else {
+			$param_str = implode(" ",$params);
+			$response = curl_get_contents("https://artii.herokuapp.com/make?text=" . urlencode($param_str));
+			$conn->message($pl['from'], "\n" . $response, $pl['type']);
+		}
 	} else {
-		$conn->message($pl['from'], "Usage: #shout <words, yo>", $pl['type']);
+		$conn->message($pl['from'], "Usage: #shout [--fonts|--font=font] <words, yo>", $pl['type']);
 	}
 }
 ?>
