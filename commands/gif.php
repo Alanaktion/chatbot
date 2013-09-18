1d1b9af414ddf4c5b3eaa6cb8e6bc4f5b35e8199
Alan Hardman <me@imalan.tk>
2013-09-18 16:01:34 -0600

    Adding some randomization to #gif results.

--- commands/gif.php
+++ commands/gif.php
@@ -2,10 +2,11 @@
 $commands['gif'] = function(&$conn, $event, $params) {
 	if(!empty($params[0])) {
 		$param_str = implode(" ",$params);
-		$url = "http://api.giphy.com/v1/gifs/search?q=" . urlencode($param_str) . "&limit=1&api_key=dc6zaTOxFJmzC";
+		$url = "http://api.giphy.com/v1/gifs/search?q=" . urlencode($param_str) . "&limit=5&api_key=dc6zaTOxFJmzC";
 		$result = Unirest::get($url);
-		if(!empty($result->body->data) && !empty($result->body->data[0]->images)) {
-			$img = $result->body->data[0]->images->original;
+		if(!empty($result->body->data) && !empty($result->body->data[0])) {
+			$i = array_rand($result->body->data);
+			$img = $result->body->data[$i]->images->original;
 			$conn->message($event['from'], $img->url, $event['type']);
 		} else {
 			$conn->message($event['from'], "Nada.", $event['type']);
