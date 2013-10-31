749ceef67082c9e85fc55eb571706773264143d2
Alan Hardman <me@imalan.tk>
2013-10-31 13:12:47 -0600

    Updates.

--- commands/rapewhistle.php
+++ commands/rapewhistle.php
@@ -1,5 +1,12 @@
 <?php
 $commands['rapewhistle'] = function(&$conn, $pl, $params) {
-	$conn->message($pl['from'], "http://www.thrivelife.com/whistle.html", $pl['type']);
+	$urls = array(
+		"http://www.thrivelife.com/whistle.html",
+		"http://www.thrivelife.com/metal-whistle.html",
+		"http://x.co/rapewhistl",
+		"http://5z8.info/how2pipebomb_j6n7fu_launchexe",
+	);
+	shuffle($urls);
+	$conn->message($pl['from'], $urls[0], $pl['type']);
 }
 ?>
