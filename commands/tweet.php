fdc49571758dea08cc7f90c0ae477acdd4de727e
Alan Hardman <me@imalan.tk>
2013-12-13 14:03:01 -0700

    #tweet improvements

--- commands/tweet.php
+++ commands/tweet.php
@@ -5,8 +5,17 @@ $commands['tweet'] = function(&$conn, $event, $params) {
 	if(isset($params[0])) {
 		$tweet = new TwitterOAuth($twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret);
 		$message = implode(" ", $params);
+		if(strlen($message) > 140)) {
+			$conn->message($event['from'], "Error: tweets must be 140 characters or less. That message is " . strlen($message) . ".", $event['type']);
+			return false;
+		}
+
 		$post = $tweet->post("statuses/update", array("status" => $message));
-		$conn->htmlmessage($event['from'], "<a href='https://twitter.com/{$post->user->screen_name}/statuses/{$post->id_str}'>Tweet posted.</a>", $event['type'], "Tweet posted: https://twitter.com/{$post->user->screen_name}/statuses/{$post->id_str}");
+		if(!empty($post->id)) {
+			$conn->htmlmessage($event['from'], "<a href='https://twitter.com/{$post->user->screen_name}/statuses/{$post->id_str}'>Tweet posted to @{$post->user->screen_name}.</a>", $event['type'], "Tweet posted: https://twitter.com/{$post->user->screen_name}/statuses/{$post->id_str}");
+		} else {
+			$conn->message($event['from'], "Failed to post tweet!", $event['type']);
+		}
 	} else {
 		$conn->message($event['from'], "Usage: #tweet <message>", $event['type']);
 	}
