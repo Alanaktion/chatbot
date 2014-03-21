afe00e565211e185125801be831fc76ac76aaf29
Alan Hardman <ahardman@thrivelife.com>
2014-03-21 16:23:47 -0600

    Adding #borat

---
+++ commands/borat.php
@@ -0,0 +1,19 @@
+<?php
+include_once dirname(__FILE__) . '/../lib/twitteroauth.php';
+$commands['borat'] = function(&$conn, $event, $params) {
+	global $twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret;
+
+	$twitter = new TwitterOAuth($twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret);
+	$tweets = $twitter->get("statuses/user_timeline", array("screen_name" => "DEVOPS_BORAT"));
+
+	$tweet = $tweets[array_rand($tweets)];
+
+	if($tweet) {
+		$conn->message($event['from'], $tweet->text, $event['type']);
+	} else {
+		print_r($tweets);
+		$conn->message($event['from'], "Unable to load tweet :(", $event['type']);
+	}
+
+}
+?>
