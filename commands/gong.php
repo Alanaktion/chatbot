f678a96af231205ec37de0eacc2aab51e7d33046
Alan Hardman <ahardman@thrivelife.com>
2014-04-24 16:04:54 -0600

    Updating #gong link

--- commands/gong.php
+++ commands/gong.php
@@ -1,5 +1,5 @@
 <?php
 $commands['gong'] = function(&$conn, $pl, $params) {
-	$conn->message($pl['from'], "http://www.hark.com/clips/nxhncnjrqy-gong", $pl['type']);
+	$conn->message($pl['from'], "http://x.co/gonnng", $pl['type']);
 }
 ?>
