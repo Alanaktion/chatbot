38c455f7eca7c6a1eeff83e309c06f833572de95
Alan Hardman <ahardman@thrivelife.com>
2014-04-24 10:11:49 -0600

    Adding #shout2 command

---
+++ commands/shout2.php
@@ -0,0 +1,11 @@
+<?php
+$commands['shout2'] = function(&$conn, $event, $params) {
+	if(empty($params)) {
+		$conn->message($event['from'], "Usage: #shout2 <message>", $event['type']);
+	} elseif ($params[0]{0} == "#") {
+		$conn->message($event['from'], "Wat.", $event['type']);
+	} else {
+		$conn->htmlmessage($event['from'], "<p><span style='font-size: xx-large; font-weight: bold; color:yellow; background: #444;'>" . htmlentities(implode(" ", $params)) . "</span></p>", $event['type']);
+	}
+}
+?>
