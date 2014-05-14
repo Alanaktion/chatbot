2cb44000afdd7959100864b8661bf7b500bc5361
Alan Hardman <ahardman@thrivelife.com>
2014-05-14 15:07:21 -0600

    Adding #anagram, updating commands/badwords list

--- commands/cat.php
+++ commands/cat.php
@@ -3,14 +3,16 @@ $commands['cat'] = function(&$conn, $event, $params) {
 	try {
 		if (!empty($params[0])) {
 			$categories = array("hats","space","funny","sunglasses","boxes","caturday","ties","dream","kittens","sinks","clothes");
-			if($params[0] == "gif") {
+			if($params[0] == "gif" && empty($params[1])) {
 				$cat_xml = curl_get_contents("http://thecatapi.com/api/images/get?format=xml&type=gif");
 				$cat = new SimpleXMLElement($cat_xml);
 				$conn->message($event['from'], $cat->data->images->image->url, $event['type']);
 			} elseif(in_array($params[0],$categories)) {
 				$url = "http://thecatapi.com/api/images/get?format=xml&category=" . urlencode($params[0]);
-				if(!empty($params[1]) && $params[1] == "gif")
+				if(!empty($params[1]) && $params[1] == "gif") {
 					$url .= "&type=gif";
+				}
+
 				$cat_xml = curl_get_contents($url);
 				$cat = new SimpleXMLElement($cat_xml);
 				if(!empty($cat->data->images)) {
--- commands/nslookup.php
+++ commands/nslookup.php
@@ -1,7 +1,7 @@
 <?php
 $commands['nslookup'] = function(&$conn, $event, $params) {
 	if(!empty($params[0])) {
-		$result = json_decode(curl_get_contents("http://alanaktion.net/tools/nslookup_api.php?host=" . urlencode($params[0])));
+		$result = json_decode(curl_get_contents("http://www.anagramica.com/all/" . urlencode($params[0])));
 		if(!empty($result)) {
 			$conn->message($event['from'], implode(", ", $result), $event['type']);
 		} else {
