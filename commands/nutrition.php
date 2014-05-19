a56970f215711b7dca2543e173280d159359ab97
Shane S Loveland <shaneloveland@gmail.com>
2014-05-19 13:25:27 -0600

    make it work with multiple words

--- commands/nutrition.php
+++ commands/nutrition.php
@@ -1,12 +1,12 @@
 <?php
 $commands['nutrition'] = function(&$conn, $event, $params) {
 	if(!empty($params[0])) {
-		$params[0] = strtolower($params[0]);
-		if($params[0] == "pizza") {
+		$search = strtolower(implode($params));
+		if($search == "pizza") {
 			$conn->message($event['from'], "Bad", $event['type']);
 			return;
 		}
-		$result =file_get_contents("https://api.nutritionix.com/v1_1/search/" . urlencode($params[0]) . "?results=0:1&fields=item_name,brand_name,nf_calories,nf_total_fat,nf_cholestorol,nf_sodium_nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_vitamin_a,nf_vitamin_c,nf_calcium,nf_iron&appId=39f195bb&appKey=580ad3c3d167b790a9bc80e4a9f0b299");
+		$result =file_get_contents("https://api.nutritionix.com/v1_1/search/" . urlencode($search) . "?results=0:1&fields=item_name,brand_name,nf_calories,nf_total_fat,nf_cholestorol,nf_sodium_nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_vitamin_a,nf_vitamin_c,nf_calcium,nf_iron&appId=39f195bb&appKey=580ad3c3d167b790a9bc80e4a9f0b299");
 		$result = json_decode($result);
 		if(!empty($result->hits[0])) {
 			$name = $result->hits[0]->fields->item_name;
