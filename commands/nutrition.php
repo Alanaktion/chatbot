bfca007ed99cebc7649212278863404244ef253d
Shane S Loveland <shaneloveland@gmail.com>
2014-05-19 13:03:48 -0600

    asdf

--- commands/nutrition.php
+++ commands/nutrition.php
@@ -7,7 +7,7 @@ $commands['nutrition'] = function(&$conn, $event, $params) {
 			return;
 		}
 		$result =file_get_contents("https://api.nutritionix.com/v1_1/search/" . urlencode($params[0]) . "?results=0:1&fields=item_name,brand_name,nf_calories,nf_total_fat,nf_cholestorol,nf_sodium_nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein,nf_vitamin_a,nf_vitamin_c,nf_calcium,nf_iron&appId=39f195bb&appKey=580ad3c3d167b790a9bc80e4a9f0b299");
-		$result = substr($result, 0, -1);
+		/*$result = substr($result, 0, -1);
 		$result = json_decode($result);
 		if(!empty($result->hits[0])) {
 			$name = $result->hits[0]->fields->item_name;
@@ -18,12 +18,12 @@ $commands['nutrition'] = function(&$conn, $event, $params) {
 			$sugars = $result->hits[0]->fields->nf_sugars;
 			$protein = $result->hits[0]->fields->nf_protein;
 
-			$html_text = "<b>Name: </b>{$name} <br /> <b>Brand: </b>{$brand} <br /> <b>Calories: </b>{$calories} <br /> <b>Protein: </b>{$protein}g<br /> <b>Fat: </b>{$fat}g <br /> <b>Fiber: </b>{$fiber}g <br /> <b>Sugars:</b> {$sugars}g <br /> ";
+			$html_text = "<b>Name: </b>{$name} <br /> <b>Brand: </b>{$brand} <br /> <b>Calories: </b>{$calories} <br /> <b>Protein: </b>{$protein}g<br /> <b>Fat: </b>{$fat}g <br /> <b>Fiber: </b>{$fiber}g <br /> <b>Sugars:</b> {$sugars}g <br /> ";*/
 
-			$conn->htmlmessage($event['from'], $html_text, $event['type']);
+			$conn->htmlmessage($event['from'], $result, $event['type']);/*
 		} else {
 			$conn->message($event['from'], "No nutritional data found.", $event['type']);
-		}
+		}*/
 	} else {
 		$conn->message($event['from'], "Usage: #nutrition <food>", $event['type']);
 	}
