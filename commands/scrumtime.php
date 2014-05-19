edb2aa5402b7c7399336d05457bc5160416e7b0a
Shane S Loveland <shaneloveland@gmail.com>
2014-05-19 10:03:44 -0600

    push... junk

--- commands/scrumtime.php
+++ commands/scrumtime.php
@@ -1,41 +1,40 @@
 <?php
 
-$colors = array(
-	'ff00ff',
-	'ff00cc',
-	'ff0099',
-	'ff0066',
-	'ff0033',
-	'ff0000',
-	'ff3300',
-	'ff6600',
-	'ff9900',
-	'ffcc00',
-	'ffff00',
-	'ccff00',
-	'99ff00',
-	'66ff00',
-	'33ff00',
-	'00ff00',
-	'00ff33',
-	'00ff66',
-	'00ff99',
-	'00ffcc',
-	'00ffff',
-	'00ccff',
-	'0099ff',
-	'0066ff',
-	'0033ff',
-	'0000ff',
-	'3300ff',
-	'6600ff',
-	'9900ff',
-	'cc00ff'
-);
-
-$i = 0;
-
 $commands['scrumtime'] = function(&$conn, $pl, $params) {
+	$colors = array(
+		'ff00ff',
+		'ff00cc',
+		'ff0099',
+		'ff0066',
+		'ff0033',
+		'ff0000',
+		'ff3300',
+		'ff6600',
+		'ff9900',
+		'ffcc00',
+		'ffff00',
+		'ccff00',
+		'99ff00',
+		'66ff00',
+		'33ff00',
+		'00ff00',
+		'00ff33',
+		'00ff66',
+		'00ff99',
+		'00ffcc',
+		'00ffff',
+		'00ccff',
+		'0099ff',
+		'0066ff',
+		'0033ff',
+		'0000ff',
+		'3300ff',
+		'6600ff',
+		'9900ff',
+		'cc00ff'
+	);
+
+	$i = 0;
 	$conn->htmlmessage($pl['from'], "<p style='font-size: xx-large; font-weight: bold; color:"+$colors[$i] +"; background: #000;'>&nbsp;&nbsp;S&nbsp;&nbsp;&nbsp;</p>", $pl['type']);
 	sleep(1);
 	$i++;
