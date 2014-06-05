934d6bb1269b62417055f36b5ee1412778331465
Ryan Bouche <ryan@trailjack.com>
2014-06-05 15:00:50 -0600

    html

--- commands/loic.php
+++ commands/loic.php
@@ -3,7 +3,7 @@ $commands['loic'] = function(&$conn, $event, $params) {
 	if (!empty($params[0])) {
 		$conn->message($event['from'], "Targeting...", $event['type']);	
 		sleep(2);
-		$conn->message($event['from'], "<a href='http://0.0.0.0'>{$params[0]}</a> has been destroyed.", $event['type']);	
+		$conn->htmlmessage($event['from'], "<a href='http://0.0.0.0'>{$params[0]}</a> has been destroyed.", $event['type']);	
 		
 	} else {
 		$conn->message($event['from'], "Please enter a website to target", $event['type']);
