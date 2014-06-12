15ace381d5a3f0c6bdf0b9ce2b006a3a34a78627
Alan Hardman <ahardman@thrivelife.com>
2014-06-12 11:35:37 -0600

    Making #nslookup actually do an nslookup

--- commands/nslookup.php
+++ commands/nslookup.php
@@ -1,9 +1,9 @@
 <?php
 $commands['nslookup'] = function(&$conn, $event, $params) {
 	if(!empty($params[0])) {
-		$result = json_decode(curl_get_contents("http://www.anagramica.com/all/" . urlencode($params[0])));
+		$result = gethostbyname($params[0]);
 		if(!empty($result)) {
-			$conn->message($event['from'], implode(", ", $result), $event['type']);
+			$conn->message($event['from'], $result, $event['type']);
 		} else {
 			$conn->message($event['from'], "No matching hosts found.", $event['type']);
 		}
