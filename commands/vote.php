6c34047f10c37dc65058abbc8cc41b2e346f6764
Ryan Bouche <ryan@trailjack.com>
2014-06-26 13:05:18 -0600

    fb icon vote command

---
+++ commands/vote.php
@@ -0,0 +1,17 @@
+<?php
+$commands['rand'] = function(&$conn, $event, $params) {
+	
+	$val = rand(0,1);
+	if($val == 1) {
+		$response = "(y) Yes";
+	} else {
+		$response = "👎 No";
+	}
+	
+	if(isset($response)) {
+		$conn->message($event['from'], $response, $event['type']);
+	} else {
+		$conn->message($event['from'], "Usage: #vote", $event['type']);
+	}
+}
+?>
