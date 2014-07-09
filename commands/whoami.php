f20f27c7a57e155945fb3c63ac1a703ec5f8db9e
Alan Hardman <ahardman@thrivelife.com>
2014-07-09 15:50:03 -0600

    Adding random links to #whois

--- commands/whoami.php
+++ commands/whoami.php
@@ -1,5 +1,10 @@
 <?php
 $commands['whoami'] = function(&$conn, $pl, $params) {
-	$conn->htmlmessage($pl['from'], "<a href='http://bit.ly/GQ5vfd'>" . $pl['realfrom'] . "</a>", $pl['type'], $pl['realfrom']);
+	$links = array(
+		"https://www.youtube.com/watch?v=AK2B5ffWR6g&t=10",
+		"http://bit.ly/GQ5vfd"
+	);
+	$i = rand(0,1);
+	$conn->htmlmessage($pl['from'], "<a href='{$links[$i]}'>" . $pl['realfrom'] . "</a>", $pl['type'], $pl['realfrom']);
 }
 ?>
