206d9128497ee4c906fc1fa7ae1fc6518f2878c1
Alan Hardman <ahardman@thrivelife.com>
2014-09-08 15:21:37 -0600

    Fixing #vote emoticons in Pidgin

--- commands/vote.php
+++ commands/vote.php
@@ -1,13 +1,13 @@
 <?php
 $commands['vote'] = function(&$conn, $event, $params) {
-	
+
 	$val = rand(0,1);
 	if($val == 1) {
-		$response = "<a href='http://xkcd.com/yes/'>(y) Yes</a>";
+		$response = "(y) <a href='http://xkcd.com/yes/'>Yes</a>";
 	} else {
-		$response = "<a href='http://xkcd.com/no/'>👎 No</a>";
+		$response = "👎 <a href='http://xkcd.com/no/'>No</a>";
 	}
-	
+
 	if(isset($response)) {
 		$conn->htmlmessage($event['from'], $response, $event['type']);
 	} else {
