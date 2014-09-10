3142425cc6a68bf18dddaef2d6a085433f35bf2e
Alan Hardman <ahardman@thrivelife.com>
2014-09-10 10:24:13 -0600

    Improve #borat performance and random sample size

--- commands/borat.php
+++ commands/borat.php
@@ -1,17 +1,20 @@
 <?php
 include_once dirname(__FILE__) . '/../lib/twitteroauth.php';
 $commands['borat'] = function(&$conn, $event, $params) {
-	global $twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret;
+	global $twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret, $devops_borat_feed;
 
-	$twitter = new TwitterOAuth($twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret);
-	$tweets = $twitter->get("statuses/user_timeline", array("screen_name" => "DEVOPS_BORAT", "trim_user" => true, "count" => 50));
+	if(empty($devops_borat_feed)) {
+		$twitter = new TwitterOAuth($twitterConsumerKey, $twitterConsumerSecret, $twitterOAuthToken, $twitterOAuthSecret);
+		$devops_borat_feed = $twitter->get("statuses/user_timeline", array("screen_name" => "DEVOPS_BORAT", "trim_user" => true, "count" => 3000));
+	}
 
-	$tweet = $tweets[array_rand($tweets)];
+	$tweet = $devops_borat_feed[array_rand($devops_borat_feed)];
 
 	if($tweet) {
 		$conn->message($event['from'], $tweet->text, $event['type']);
 	} else {
-		print_r($tweets);
+		print_r($devops_borat_feed);
+		unset($devops_borat_feed);
 		$conn->message($event['from'], "Unable to load tweet :(", $event['type']);
 	}
 
