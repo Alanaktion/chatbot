3ea7a6a97d04efdd76f590753d7499225fdaeb00
Brian Pratt <bpratt@thrivelife.com>
2014-09-23 17:03:26 -0600

    pushing neal;s crap

--- commands/rebuild.php
+++ commands/rebuild.php
@@ -3,7 +3,7 @@ $commands['rebuild'] = function(&$conn, $event, $params) {
 	if (!empty($params[0])) {
 		$conn->message($event['from'], "Rebuilding...", $event['type']);	
 		sleep(2);
-		$conn->htmlmessage($event['from'], "<a href='http://0.0.0.0'>{$params[0]}</a> has been rebuilt.", $event['type']);	
+		$conn->htmlmessage($event['from'], "<a href='http://0.0.0.0'>{$params[0]}</a> has been rebuilt from a loic blast", $event['type']);	
 		
 	} else {
 		$conn->message($event['from'], "Please enter a website to rebuild", $event['type']);
