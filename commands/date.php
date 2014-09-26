a4b659f919549c23b8525804180211712f61ebb3
Alan Hardman <ahardman@thrivelife.com>
2014-09-26 15:16:02 -0600

    Fix #date timezone

--- commands/date.php
+++ commands/date.php
@@ -1,5 +1,7 @@
 <?php
 $commands['date'] = function(&$conn, $event, $params) {
+	global $timezone;
+	date_default_timezone_set($timezone);
 	if(!empty($params)) {
 		$conn->message($event['from'], date(implode(" ", $params)), $event['type']);
 	} else {
