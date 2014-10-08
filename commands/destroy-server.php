6ede9712e36c77a61d27c0c74e0db8a1c5cd5cdc
Brian Pratt <bpratt@thrivelife.com>
2014-10-08 15:18:45 -0600

    Pushing more crap lol blake

--- commands/destroy-server.php
+++ commands/destroy-server.php
@@ -1,9 +1,9 @@
 <?php
 $commands['destroy-server'] = function(&$conn, $event, $params) {
 	if (!empty($params[0])) {
-		$conn->message($event['from'], "Destroying...", $event['type']);	
+		$conn->message($event['from'], "Destroying $params[0]", $event['type']);	
 		sleep(2);
-		$conn->htmlmessage($event['from'], "<a href='http://0.0.0.0'>{$params[0]}</a> has been destroy rm -Rf / command", $event['type']);			
+		$conn->htmlmessage($event['from'], "The server named <a href='http://0.0.0.0'>{$params[0]}</a> has been destroy using the rm -Rf / command", $event['type']);			
 	} else {
 		$conn->message($event['from'], "Please enter a server to destroy", $event['type']);
 	}
