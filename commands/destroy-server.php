014b7d50872c7b8a40ed129d65b3200a7f5445fa
Brian Pratt <bpratt@thrivelife.com>
2014-10-08 15:12:46 -0600

    pushing crap

--- commands/destroy-server.php
+++ commands/destroy-server.php
@@ -1,12 +1,12 @@
 <?php
-$commands['destory-server'] = function(&$conn, $event, $params) {
+$commands['destroy-server'] = function(&$conn, $event, $params) {
 	if (!empty($params[0])) {
-		$conn->message($event['from'], "Destorying...", $params[0]);	
+		$conn->message($event['from'], "Destroying...", $params[0]);	
 		sleep(2);
-		$conn->htmlmessage($event['from'], "$params[0] has been destory rm -Rf / command", $event['type']);	
+		$conn->htmlmessage($event['from'], "$params[0] has been destroy rm -Rf / command", $event['type']);	
 		
 	} else {
-		$conn->message($event['from'], "Please enter a server to destory", $event['type']);
+		$conn->message($event['from'], "Please enter a server to destroy", $event['type']);
 	}
 }
 ?>
