7f6f7feadede9a3b8fba9d63c2fe96ba778f110f
Alan Hardman <ahardman@thrivelife.com>
2014-12-10 15:31:06 -0700

    Cleaning up #domain

--- commands/domain.php
+++ commands/domain.php
@@ -1,8 +1,6 @@
 <?php
 $commands['domain'] = function(&$conn, $event, $params) {
 	if (!empty($params[0])) {
-		//$response = curl_get_contents("http://alanaktion.net/tools/domains_api.php?domain=" . urlencode(implode(',',$params)));
-		//$conn->message($event['from'], trim($response), $event['type']);
 		$response = Unirest::get("http://domai.nr/api/json/info?q=" . urlencode($params[0]));
 		print_r($response->body);
 		if(!empty($response->body->error_message)) {
