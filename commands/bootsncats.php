75ba79f247e09dbc44cc39d2bc4a305c7301a8e5
Alan Hardman <ahardman@thrivelife.com>
2014-12-17 10:52:27 -0700

    Adding #bootsncats

---
+++ commands/bootsncats.php
@@ -0,0 +1,17 @@
+<?php
+$commands['bootsncats'] = function(&$conn, $event, $params) {
+	$str = "boots and cats";
+	$cat = " and ";
+	if(empty($params)) {
+		$conn->message($event['from'], "Usage: #bootsncats <count>", $event['type']);
+	} elseif ($params[0] > 200) {
+		$conn->message($event['from'], "Nope :P", $event['type']);
+	} else {
+		$arr = array();
+		for($i = 0; $i < $params[0]; $i++) {
+			$arr[] = $str;
+		}
+		$conn->message($event['from'], implode($cat, $arr), $event['type']);
+	}
+}
+?>
