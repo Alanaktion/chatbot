085ee3f1ffb77be2682402c7697c2c97fa57285a
Alan Hardman <alanaktion@gmail.com>
2015-01-02 20:09:30 -0700

    New #math!

--- commands/1337.php
+++ commands/1337.php
@@ -1,12 +1,12 @@
 <?php
-$commands['1337'] = function(&$conn, $pl, $params) {
+$commands['1337'] = function(&$conn, $event, $params) {
 	global $mash_key;
 	if (!empty($params[0])) {
 		$param_str = implode(" ",$params);
 		$response = Unirest::get("https://montanaflynn-l33t-sp34k.p.mashape.com/encode?text=" . urlencode($param_str), array("X-Mashape-Authorization" => $mash_key));
-		$conn->message($pl['from'], $response->raw_body, $pl['type']);
+		$conn->message($event['from'], $response->raw_body, $event['type']);
 	} else {
-		$conn->message($pl['from'], "Usage: #1337 <sentence>", $pl['type']);
+		$conn->message($event['from'], "Usage: #1337 <sentence>", $event['type']);
 	}
 }
 ?>
--- commands/math.php
+++ commands/math.php
@@ -1,49 +1,12 @@
 <?php
-include_once dirname(__FILE__) . '/../lib/evalmath.class.php';
 $commands['math'] = function(&$conn, $event, $params) {
-	global $math;
-	if(isset($params[0])) {
-		// Reset EvalMath
-		if($params[0] == "reset" || $params[0]=="clear") {
-			unset($math);
-			$conn->message($event['from'], "Calculator reset.", $event['type']);
-			return;
-		}
-
-		// Ensure $math is defined
-		if(!isset($math)) {
-			$math = new EvalMath();
-		}
-
-		// Show user defined variables and functions
-		if($params[0] == "vars") {
-			$vars = $math->vars();
-			$conn->message($event['from'], print_r($vars, true), $event['type']);
-			return;
-		}
-		if($params[0] == "funcs") {
-			$funcs = $math->funcs();
-			$conn->message($event['from'], print_r($funcs, true), $event['type']);
-			return;
-		}
-
-		$expr = str_replace(",", "", implode(" ", $params));
-		$result = $math->evaluate($expr);
-		if($result === false) {
-			$conn->htmlmessage($event['from'], "<p style='color: red;'>Error: " . htmlentities($math->last_error) . "</p>", $event['type']);
-		} elseif(is_nan($result)) {
-			$conn->htmlmessage($event['from'], "<p style='color: #a7a32d; font-weight: bold;'>NaN</p>", $event['type']);
-		} elseif($result > 1000) {
-			if($result > 9000 && $result < 10000) {
-				$conn->message($event['from'], "OVER 9000! (" . number_format($result) . ")", $event['type']);
-			} else {
-				$conn->message($event['from'], number_format($result), $event['type']);
-			}
-		} else {
-			$conn->message($event['from'], $result, $event['type']);
-		}
+	global $mash_key;
+	if (!empty($params[0])) {
+		$param_str = implode(" ",$params);
+		$response = Unirest::get("https://alanaktion-qalc-v1.p.mashape.com/?q=" . urlencode($param_str), array("X-Mashape-Authorization" => $mash_key));
+		$conn->message($event['from'], $response->raw_body, $event['type']);
 	} else {
-		$conn->message($event['from'], "Usage: #math <expression|clear|reset>", $event['type']);
+		$conn->message($event['from'], "Usage: #math <expression>", $event['type']);
 	}
 }
 ?>
